2c4245f81431850b0b61fa0e4b4bddf781fc0f76
GuilhermeGiusti <guilherme.giusti04@gmail.com>
2025-11-26 12:38:42 -0300

    secret vault

--- pkg/vars/values.go
+++ pkg/vars/values.go
@@ -2,12 +2,41 @@ package vars
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 	"strings"
 )
 
-func GetStringValue(param string) string {
+type (
+	Configuration interface {
+		GetStringValue(param string) string
+		GetIntValue(param string) int
+		GetStringValueDefault(param string, _default string) string
+		GetIntValueDefault(param string, _default int) int
+		GetFloat64Value(param string) float64
+		GetFloat64ValueDefault(param string, _default float64) float64
+		IsDebugEnabled() bool
+		IsProduction() bool
+	}
+
+	configuration struct {
+		repository string
+		context    string
+	}
+)
+
+func NewConfiguration(repository string, context string) Configuration {
+	return &configuration{
+		repository: repository,
+		context:    context,
+	}
+}
+
+func (c *configuration) GetStringValue(param string) string {
+	if v, ok := c.readSecretStoreVar(param); ok {
+		return v
+	}
 	if val := os.Getenv(param); val != "" {
 		return val
 	}
@@ -17,14 +46,16 @@ func GetStringValue(param string) string {
 	panic("environment variable " + param + " is not set")
 }
 
-func getIntValue(param string) *int {
-	val := os.Getenv(param)
-	if val == "" {
-		if v, ok := readDotEnvVar(param); ok {
-			val = v
-		} else {
-			return nil
-		}
+func (c *configuration) getIntValue(param string) *int {
+	var val string
+	if v, ok := c.readSecretStoreVar(param); ok {
+		val = v
+	} else if v := os.Getenv(param); v != "" {
+		val = v
+	} else if v, ok := readDotEnvVar(param); ok {
+		val = v
+	} else {
+		return nil
 	}
 	intVal, err := strconv.Atoi(val)
 	if err != nil {
@@ -33,15 +64,18 @@ func getIntValue(param string) *int {
 	return &intVal
 }
 
-func GetIntValue(param string) int {
-	val := getIntValue(param)
+func (c *configuration) GetIntValue(param string) int {
+	val := c.getIntValue(param)
 	if val == nil {
 		panic("environment variable " + param + " is not set")
 	}
 	return *val
 }
 
-func GetStringValueDefault(param string, _default string) string {
+func (c *configuration) GetStringValueDefault(param string, _default string) string {
+	if v, ok := c.readSecretStoreVar(param); ok {
+		return v
+	}
 	if val := os.Getenv(param); val != "" {
 		return val
 	}
@@ -51,22 +85,24 @@ func GetStringValueDefault(param string, _default string) string {
 	return _default
 }
 
-func GetIntValueDefault(param string, _default int) int {
-	val := getIntValue(param)
+func (c *configuration) GetIntValueDefault(param string, _default int) int {
+	val := c.getIntValue(param)
 	if val == nil {
 		return _default
 	}
 	return *val
 }
 
-func getFloat64Value(param string) *float64 {
-	val := os.Getenv(param)
-	if val == "" {
-		if v, ok := readDotEnvVar(param); ok {
-			val = v
-		} else {
-			return nil
-		}
+func (c *configuration) getFloat64Value(param string) *float64 {
+	var val string
+	if v, ok := c.readSecretStoreVar(param); ok {
+		val = v
+	} else if v := os.Getenv(param); v != "" {
+		val = v
+	} else if v, ok := readDotEnvVar(param); ok {
+		val = v
+	} else {
+		return nil
 	}
 	floatVal, err := strconv.ParseFloat(val, 64)
 	if err != nil {
@@ -75,23 +111,26 @@ func getFloat64Value(param string) *float64 {
 	return &floatVal
 }
 
-func GetFloat64Value(param string) float64 {
-	val := getFloat64Value(param)
+func (c *configuration) GetFloat64Value(param string) float64 {
+	val := c.getFloat64Value(param)
 	if val == nil {
 		panic("environment variable " + param + " is not set")
 	}
 	return *val
 }
 
-func GetFloat64ValueDefault(param string, _default float64) float64 {
-	val := getFloat64Value(param)
+func (c *configuration) GetFloat64ValueDefault(param string, _default float64) float64 {
+	val := c.getFloat64Value(param)
 	if val == nil {
 		return _default
 	}
 	return *val
 }
 
-func IsDebugEnabled() bool {
+func (c *configuration) IsDebugEnabled() bool {
+	if v, ok := c.readSecretStoreVar("DEBUG"); ok {
+		return strings.ToLower(strings.TrimSpace(v)) == "true"
+	}
 	if val := os.Getenv("DEBUG"); val != "" {
 		return strings.ToLower(strings.TrimSpace(val)) == "true"
 	}
@@ -101,7 +140,10 @@ func IsDebugEnabled() bool {
 	return false
 }
 
-func IsProduction() bool {
+func (c *configuration) IsProduction() bool {
+	if v, ok := c.readSecretStoreVar("ENVIRONMENT"); ok {
+		return strings.ToLower(strings.TrimSpace(v)) == "production"
+	}
 	if val := os.Getenv("ENVIRONMENT"); val != "" {
 		return strings.ToLower(strings.TrimSpace(val)) == "production"
 	}
@@ -109,6 +151,24 @@ func IsProduction() bool {
 		return strings.ToLower(strings.TrimSpace(v)) == "production"
 	}
 	return false
+}
+
+func (c *configuration) readSecretStoreVar(param string) (string, bool) {
+	repoName := c.repository
+	context := c.context
+
+	if repoName == "" || context == "" {
+		return "", false
+	}
+
+	path := fmt.Sprintf("/mnt/secrets-store/_%s_%s_%s", repoName, context, param)
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return "", false
+	}
+
+	val := strings.TrimSpace(string(data))
+	return val, true
 }
 
 func readDotEnvVar(param string) (string, bool) {
